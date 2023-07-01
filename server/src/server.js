1bab439b5c6085f4296efa67c27f59d47e1ffe7a
Akshay Bagai <100753870+Akshaybagai52@users.noreply.github.com>
2023-07-01 23:41:54 +0530

    connected backend

--- server/src/server.js
+++ server/src/server.js
@@ -1,10 +1,17 @@
 const PORT = 8000;
 const app = require('./app')
+const {loadPlanetData} = require('./models/planets.model')
 
 const http = require('http')
 
 const server = http.createServer(app);
+async function startServer() {
+    await loadPlanetData()
 
 server.listen(PORT, () => {
     console.log(`Server is running on port ${PORT}`);
 })
+
+}
+startServer()
+
