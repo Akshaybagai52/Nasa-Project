33775c1c08b31fe5889edea0e9178f2f4d0ea10c
Akshay Bagai <100753870+Akshaybagai52@users.noreply.github.com>
2023-07-01 21:34:09 +0530

    fixed cors error

--- server/src/app.js
+++ server/src/app.js
@@ -1,8 +1,9 @@
 const express = require("express");
 
 const planetsRouter = require('./routes/planets/planets.router')
-
+const cors = require('cors');
 const app = express()
 app.use(express.json())
+app.use(cors())
 app.use(planetsRouter)
 module.exports = app
