d2608e257c74148daf0e271443d70f0e38dd527b
Akshay Bagai <100753870+Akshaybagai52@users.noreply.github.com>
2023-07-02 17:38:32 +0530

    gg

--- server/src/app.js
+++ server/src/app.js
@@ -1,10 +1,13 @@
 const express = require("express");
 const path = require('path');
 const planetsRouter = require('./routes/planets/planets.router')
+const launchesRouter = require('./routes/launches/launches.router')
 const cors = require('cors');
+const morgan = require('morgan')
 const app = express()
 app.use(express.json())
 app.use(cors())
+app.use(morgan('combined'))
 
 app.use(express.static(path.join(__dirname, '..', 'public')))
 app.use(planetsRouter)
