24bd69d52e2f9adf67e44664e9ea05a64b8ba851
Akshay Bagai <100753870+Akshaybagai52@users.noreply.github.com>
2023-07-04 14:31:11 +0530

    POST

--- server/src/app.js
+++ server/src/app.js
@@ -3,15 +3,15 @@ const path = require('path');
 const planetsRouter = require('./routes/planets/planets.router')
 const launchesRouter = require('./routes/launches/launches.router')
 const cors = require('cors');
-const morgan = require('morgan')
+// const morgan = require('morgan')
 const app = express()
 app.use(express.json())
 app.use(cors())
-app.use(morgan('combined'))
+// app.use(morgan('combined'))
 
 app.use(express.static(path.join(__dirname, '..', 'public')))
-app.use(planetsRouter)
-app.use(launchesRouter)
+app.use('/planets',planetsRouter)
+app.use('/launches',launchesRouter)
 app.get('/*', (req,res) => {
     res.sendfile(path.join(__dirname, '..', 'public', 'index.html'))
 })
--- server/src/routes/launches/launches.router.js
+++ server/src/routes/launches/launches.router.js
@@ -2,8 +2,10 @@ const express = require('express');
 
 const {
     httpGetAllLaunches,
+    httpAddNewLaunches
 } = require('./launches.controller')
 const launchesRouter = express.Router();
-launchesRouter.get('/launches', httpGetAllLaunches);
+launchesRouter.get('/', httpGetAllLaunches);
+launchesRouter.post('/', httpAddNewLaunches);
 
 module.exports = launchesRouter
